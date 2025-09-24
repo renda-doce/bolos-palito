849c6c4ceb74a6a9c3413fb3abe6403a4b767d2c
Roberto Tavares <roberto.tavares@trt2.jus.br>
2025-09-24 09:30:09 -0300

    Alterações no modo de alterar o link dos botões

--- js/substitui-link-botoes.js
+++ js/substitui-link-botoes.js
@@ -1,12 +1,12 @@
 // Define o link do grupo
-  const linkGrupo = "https://wa.me/5511975037855?text=Olá%2C%20tenho%20interesse%20no%20curso!";
+  const linkBotao = "__LINK_GRUPO__";
 
   // Seleciona todos os elementos com a classe
   const botoes = document.querySelectorAll(".link-botao");
 
   // Adiciona o link em cada um
   botoes.forEach(botao => {
-    botao.setAttribute("href", linkGrupo);
+    botao.setAttribute("href", linkBotao);
     botao.setAttribute("target", "_blank"); // abre em nova aba
     botao.setAttribute("rel", "noopener noreferrer"); // segurança
   });
